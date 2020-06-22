ef18b66f92670f88ff5b1bb2c09dd983a39bcede
davids-pro <54986025+davids-pro@users.noreply.github.com>
2020-06-22 12:19:29 +0200

    backup

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-const http = require('http');
 const app = require('./app');
 
-const server = http.createServer(app);
-
-server.listen(3000, () => {
+app.listen(3000, () => {
   console.log('** NodeJS Server is listening on http://localhost:3000 **');
 });
